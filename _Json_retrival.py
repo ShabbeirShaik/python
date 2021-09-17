5694255c3a19471bc74d5ade9eba0d5b4bc1dab1
shabbeir <shabbeirshaik1234@gmail.com>
2021-09-17 20:41:16 +0530

    retriving the data from the saved json file

---
+++ _Json_retrival.py
@@ -0,0 +1,8 @@
+f=open("c://data//book.txt","r")#specify the path of the file that you want to read
+s=f.read()
+print(s)
+import json
+main_book=json.loads(s)
+print(type(main_book))
+print(main_book['sajid'])
+print(main_book['sajid']['phone'])
